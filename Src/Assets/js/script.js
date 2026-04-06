955390e3853e80d0d6805e8d1eed65eec4df1ac1
Joao Souza <jujp.souza@gmail.com>
2026-04-06 10:44:06 -0300

    feat: adição leve ao js

--- Src/Assets/js/script.js
+++ Src/Assets/js/script.js
@@ -1,1 +1,8 @@
-console.log("inicio do js");
+function cadastrar() {
+  
+    let nome = prompt("Digite o nome do vinho:");
+    if(nome === null || nome === "") {
+        alert("Operacao Cancelada.");
+        return;
+    }
+}
